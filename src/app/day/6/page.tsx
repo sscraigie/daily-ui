fc0acb8b18fd6744558deb881fc3dd16a8a59641
Spencer Craigie <sscraigie@live.com>
2023-11-08 08:52:22 -0800

    Started making Footer

--- src/app/day/6/page.tsx
+++ src/app/day/6/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Header from "./components/Header";
 import SideProfile from "./components/SideProfile";
 import { PinnedSection } from "./components/PinnedSection";
+import { Footer } from "./components/Footer";
 
 // Border: border-[#7e8590]
 // Icon: fill-[#7e8590]
@@ -13,12 +14,13 @@ export const Github = () => {
   return (
     <div className="flex h-screen flex-col bg-[#0e1117]">
       <Header />
-      <div className="flex">
+      <div className="flex h-96">
         <SideProfile />
         <div className="w-full p-2">
           <PinnedSection />
         </div>
       </div>
+      <Footer />
     </div>
   );
 };
